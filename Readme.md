6d8f49f32c35b12f749ca526fa3ade4f44def0e2
jiyoooon <wldbs9259@gmail.com>
2024-11-12 22:34:03 +0900

    Feat Add Upsert function

--- Readme.md
+++ Readme.md
@@ -13,24 +13,26 @@
 + Spark Application 구현에 사용가능한 언어는 **Scala나 Java**로 제한합니다.
 + File Path를 포함하여 제출시 특정할 수 없는 정보는 모두 임의로 작성해주세요.
 
-
-
-
 ### 시나리오
-1. 테이블생성 :  .sql 파일 읽기, CREATE EXTERNAL TABLE IF NOT EXISTS....
+1. 테이블생성 : .sql 파일 읽기, CREATE EXTERNAL TABLE IF NOT EXISTS....
 2. hivewearhouse write(snappy.parquet)
-3. delta 에도 동일하게 write
+3. delta 에도 동일하게 write - 최신 상태 유지 merge into(upsert)
 4. 배치 장애시 복구 - delta로 hivewearhouse 백업 
 
-### 제약사항 
+### 이슈 사항  
 + Hive Transaction 제약 사항
   + ORC 파일 포맷만 사용 가능 
   + Transaction (transactional=true)을 지원하는 테이블은 LOAD DATA 쿼리를 사용할 수 없음 
   + External Table의 경우 ACID 테이블을 만들 수 없음, transaction 또한 지원하지 않음
     + 해결방안 : spark 로 transaction 처리, 최종 데이터 테이블 따로 생성하고 external table 처리 
-+ Delta 복구
++ 장애시 delta 로 복구 
+  + delta 에 upsert 사용하고자 했으나  **You can preprocess the source table to eliminate the possibility of multiple matches.** 에러 발생 
+  + 원인 :소스 데이터 세트의 여러 행이 일치하고 병합이 대상 델타 테이블의 동일한 행을 업데이트하려고 시도하는 경우 작업 merge이 실패할 수 있습니다. 병합의 SQL 의미론에 따르면 이러한 업데이트 작업은 일치하는 대상 행을 업데이트하는 데 어떤 소스 행을 사용해야 하는지 명확하지 않으므로 모호합니다. 소스 테이블을 사전 처리하여 여러 일치 가능성을 제거할 수 있습니다.
+  + 해결방법 : 기존에 없는 행 insert 이후, 있는 행은 update 
 + 추가 기간 처리 대응
   + 기본 파일 쓰기는 append, 중복 데이터는 overwrite 옵션 활성화 ??  
+
+
 
 
  
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,13 @@ import io.delta.tables.DeltaTable;
 import org.apache.spark.sql.*;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 import static org.apache.spark.sql.functions.*;
 
 public class Main {
-    static String input_file_path = "/Users/jy_kim/Downloads/2019-Nov.csv";
+    static String input_file_path = "/Users/jy_kim/Downloads/2019-Dec.csv";
     static String hive_wearhouse_path = "hdfs://localhost:9000/user/hive/warehouse/";
     static String hdfs_path = "hdfs://localhost:9000/user/";
     static String hive_url = "jdbc:hive2://localhost:10000/default";
@@ -39,11 +40,13 @@ public class Main {
         builder.appName("Etl_Job");
         SparkSession sparkSession = builder.getOrCreate();
 
+
         try {
             /*
             * 1. 테이블생성 :  .sql 파일 읽기, CREATE EXTERNAL TABLE IF NOT EXISTS....
             * 2. hivewearhouse write(snappy.parquet)
             * 3. delta 에도 동일하게 write
+            * 4. 배치 작업에 장애 발생시 delta로 백업
             * */
 
             /* 1. 테이블 생성 */
@@ -53,57 +56,75 @@ public class Main {
 
             Dataset<Row> dataset = sparkSession.read().option("header", "true").csv(input_file_path)
                 .select(
-                    functions.col("event_time"),
-                    functions.col("event_type"),
-                    functions.col("product_id").cast("long"),
-                    functions.col("category_id").cast("long"),
-                    functions.col("category_code"),
-                    functions.col("brand"),
-                    functions.col("price").cast("decimal"),
-                    functions.col("user_id"),
-                    functions.col("user_session")
+                    col("event_time"),
+                    col("event_type"),
+                    col("product_id").cast("long"),
+                    col("category_id").cast("long"),
+                    col("category_code"),
+                    col("brand"),
+                    col("price").cast("decimal"),
+                    col("user_id"),
+                    col("user_session")
                 )
                 .withColumn("part_dt", date_format(col("event_time"), "yyyy-MM-dd HH:mm:ss"))
                 .withColumn("part_year", year(col("event_time")).cast("string"))
                 .withColumn("part_month", month(col("event_time")).cast("string"))
                 .withColumn("part_day", dayofmonth(col("event_time")).cast("string"))
-                .withColumn("part_hour", hour(col("event_time")).cast("string")) ;
-            // dataset.show(100, false);
+                .withColumn("part_hour", hour(col("event_time")).cast("string"))
+                .sample(false,0.2)
+                .alias("dataset");
+             dataset.show(100, false);
 
             /* 2. daily partition 처리, part_dt(KST 기준)  */
-            dataset.write()
-                .partitionBy("part_year","part_month","part_day","part_hour")
-                .mode("append")
-                .option("compression", "snappy")
-                .parquet(hive_wearhouse_path + "user_log");
+            try {
+                dataset.write()
+                    .partitionBy("part_year","part_month","part_day","part_hour")
+                    .mode("append")
+                    .option("compression", "snappy")
+                    .parquet(hive_wearhouse_path + "user_log");
+
+            } catch (Exception e){
+                dataset.write()
+                    .partitionBy("part_year","part_month","part_day","part_hour")
+                    .mode("overwrite")
+                    .option("compression", "snappy")
+                    .parquet(hive_wearhouse_path + "user_log");
+            }
 
 
-            /* 3. delta backup  */
-            dataset.write().format("delta").save(hdfs_path + "delta_log");
+             /* 3. delta backup - update & insert */
+            try {
+
+
+//             DeltaTable deltaTable1 = DeltaTable.forPath(sparkSession, delta_path);
+//            deltaTable1.toDF().show(30, false);
+//                deltaTable1.as("old_dt")
+//                    .merge(
+//                        dataset.as("new_dt"),
+//                        "new_dt.user_id = old_dt.user_id"
+//                    )
+//                    .whenMatched("new_dt.event_time = old_dt.event_time and  old_dt.event_type = new_dt.event_type and old_dt.product_id=new_dt.product_id").updateAll().whenNotMatched().insertAll().execute();
+
+            DeltaTable deltaTable = DeltaTable.forPath(sparkSession, delta_path);
+            deltaTable.as("old_dt")
+                    .merge(
+                        dataset.as("new_dt"),
+                        "new_dt.event_time = old_dt.event_time and new_dt.user_id = old_dt.user_id and old_dt.event_type = new_dt.event_type"
+                    )
+                    .whenNotMatched().insertAll().execute();
+            } catch (Exception e){
+                dataset.write().mode("overwrite")
+                    .format("delta")
+                    .save(hdfs_path + "delta_log");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-
-            /* 장애 복구 - delta
-            * 마지막것 전체 overwrite ??
-            *   */
-
-            /* delta test */
-//            DeltaTable deltaTable = DeltaTable.forPath(sparkSession, delta_path);
-//            deltaTable.delete(functions.col("event_type").equalTo("cart"));
-
-            Dataset<Row> delta_ds = sparkSession.read().format("delta")
-                    .load(delta_path);
-
-            delta_ds.show(10, false) ;
+            /* 4. 델타 백업 */
+            Dataset<Row> delta_ds = sparkSession.read().format("delta").load(delta_path);
             delta_ds.write().partitionBy("part_year","part_month","part_day","part_hour")
                     .mode("overwrite")
                     .option("compression", "snappy")
                     .parquet(hive_wearhouse_path + "user_log");
-//            Dataset<Row> dt1 = sparkSession.read().parquet("hdfs://localhost:9000/user/hive/warehouse/user_log");
-//            dt1.show(100, false);
-
-        } finally {
-
         }
 
     }
